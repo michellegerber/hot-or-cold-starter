86ec9a830c95813824c52a4b30a7be7f860a1ae3
michellegerber <michellegerber03@gmail.com>
2015-10-12 18:56:28 +0000

    adding files and change to js

--- js/app.js
+++ js/app.js
@@ -12,6 +12,15 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-});
 
-
+    var secretNumber;
+    var newGame = function() {
+        randomNumber100();
+    };
+//When a new game starts, the program will generate a secret number between 1 and 100 that the user must guess.
+    var randomNumber100 = function() {
+    secretNumber = Math.floor(Math.random()*100);
+    }
+    randomNumber100();
+    
+})
